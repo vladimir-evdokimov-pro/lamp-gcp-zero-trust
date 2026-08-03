a96c7706c6e21da29768a48094fd95fdf56b9edf
vladimir.evdokimov <vladimir.evdokimov@s3ns.io>
2026-08-03 15:23:15 +0200

    Complete the beginning about providers configuration with variables and tfvars example file

---
+++ terraform/main.tf
@@ -0,0 +1,7 @@
+resource "google_project_service" "apis" {
+  for_each = var.gcp_apis
+
+  project = var.project_id
+  service = each.key
+  disable_on_destroy = false
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,19 @@
 variable "project_id" {
   description = "The GCP Project ID"
   type = string
-  default = "lamp-3tier-production"
 }
 
 variable "region" {
   description = "The GCP target region"
   type = string
-  default = "europe-west9"
 }
 
 variable "zone" {
   description = "The GCP target zone"
   type = string
-  default = "europe-west9-a"
+}
+
+variable "gcp_apis" {
+  description = "List of APIs to enable for realize this configuration"
+  type = set(string)
 }
