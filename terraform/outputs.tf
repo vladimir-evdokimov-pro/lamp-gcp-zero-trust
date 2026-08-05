b3b3a3f44051b3a30aa69e9b1943704b24e5f658
vladimir.evdokimov <vladimir.evdokimov@s3ns.io>
2026-08-05 17:34:22 +0200

    Add output configuration file for principal main terraform

---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "load_balancer_ip" {
+  description = "Public IP for web application access"
+  value       = module.load-balancer.lb_ip
+}
+
+output "web_vm_private_ip" {
+  description = "Private IP of the web instance"
+  value       = module.compute.web_vm_ip
+}
+
+output "app_vm_private_ip" {
+  description = "Private IP of the app instance"
+  value       = module.compute.app_vm_ip
+}
+
+output "db_psc_endpoint_ip" {
+  description = "Private IP of the Private Service Connect endpoint for Cloud SQL"
+  value       = module.database.psc_endpoint_ip
+}
+
+output "db_secret_id" {
+  description = "Secret Manager secret ID/name containing DB credentials"
+  value       = module.security.db_secret_id
+}
