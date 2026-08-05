45f91b8987555c5a784e458ce91ed3b18c2e92ec
vladimir.evdokimov <vladimir.evdokimov@s3ns.io>
2026-08-05 11:25:09 +0200

    Add database module for deployment configuration

---
+++ terraform/modules/database/outputs.tf
@@ -0,0 +1,19 @@
+output "db_private_ip" {
+  description = "Private IP about SQL instance"
+  value = google
+}
+
+output "db_name" {
+  description = "Database name"
+  value = google_sql_database.pmadb.name
+}
+
+output "db_user" {
+  description = "Database username"
+  value = google_sql_user.pmausr.name
+}
+
+output "db_private_ip" {
+  description = "Private IP about SQL instance"
+  value       = google_compute_address.psc_ip.address
+}
