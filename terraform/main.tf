28f7367997552f97d8b4f53b63e2ba3dc65fd5f5
vladimir.evdokimov <vladimir.evdokimov@s3ns.io>
2026-08-05 15:22:52 +0200

    Add principal main for deply full stack

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,4 +5,34 @@ resource "google_project_service" "apis" {
   service = each.key
   disable_on_destroy = false
 }
+module "network" {
+  source = "./modules/network"
+  project_id = var.project_id
+  region = var.region
+}
+module "security" {
+  source = "./modules/security"
+  project_id = var.project_id
+  vpc_id = module.network.vpc_id
+}
+module "compute" {
+  source = "./modules/compute"
+  project_id = var.project_id
+  network = module.network.vpc_id
+  region = var.region
+  zone = var.zone
+  sb_web_id = module.network.sb_web_id
+  sb_app_id = module.network.sb_app_id
+  sa_web_email = module.security.sa_web_email
+  sa_app_email = module.security.sa_app_email
 
+  depends_on = [ google_project_service.apis ]
+}
+module "database" {
+  source = "./modules/database"
+  project_id = var.project_id
+  region = var.region
+  vpc_id = module.network.vpc_id
+  subnet_id = module.network.sb_data_id
+  db_password = module.security.db_password
+}
